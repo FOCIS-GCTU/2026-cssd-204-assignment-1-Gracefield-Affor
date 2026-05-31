712d7719424c51d5702c2a08f6a6571df732dee6
Gracefield-Affor <2425404262@live.gctu.edu.gh>
2026-05-31 22:32:10 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+#File: initials.py
+# Description: Prints my initials in large stylized letters.
+# Assignment Number: 2
+#
+# Name: Gracefield Mawufemor Affor 
+# STUDENT ID: 2425404262
+# Email: 2425404262@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Gracefield Mawufemor Affor, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
 
-
-
-main()
+ def main():
+     #Program to display my initials 
+     print()
+     print("...GMA")
+     print()
+     print("...GGGGGGGGGGGG........MM........MM............AAAA.........")
+     print("...GGG......GGG........MMM......MMM...........AAAAAA........")
+     print("...GGG.................MM.M....M.MM..........AA....AA.......")
+     print("...GGG.................MM.MM..MM.MM.........AA......AA......")
+     print("...GGG..GGGGGGG........MM..MMMM..MM........AAAAAAAaAAAA.....")
+     print("...GGG..GG..GGG........MM...MM...MM........AA........AA.....")
+     print("...GGG......GGG........MM........MM........AA........AA.....")
+     print("...GGG......GGG........MM........MM........AA........AA.....")
+     print("...GGG......GGG...**...MM........MM...**...AA........AA...**")
+     print("...GGGGGGGGGGGG...**...MM........MM...**...AA........AA...**")
+     
+ main()
